389601c999c10f91685b0c88c0ea11d8c3a83d57
Denis <cool-di@mail.ru>
2025-04-26 13:25:00 +0300

    iter5 Переменные окружения

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,21 +2,42 @@ package config
 
 import (
 	"flag"
+	"github.com/caarlos0/env/v6"
+	"log"
 )
 
 type Config struct {
-	ServerAddress string // адрес HTTP-сервера (флаг -a)
-	BaseURL       string // базовый адрес для сокращённых ссылок (флаг -b)
+	ServerAddress string `env:"SERVER_ADDRESS"`
+	BaseURL       string `env:"BASE_URL"`
 }
 
 func NewConfig() *Config {
-	addr := flag.String("a", "localhost:8080", "адрес запуска HTTP-сервера")
-	base := flag.String("b", "http://localhost:8080", "базовый адрес для сокращённых URL")
+	cfg := &Config{
+		ServerAddress: "default.com:8080",
+		BaseURL:       "https://default.com:8080",
+	}
+
+	// Загружаем переменные окружения
+	if err := env.Parse(cfg); err != nil {
+		log.Fatalf("ошибка чтения переменных окружения: %v", err)
+	}
+
+	envAddr := cfg.ServerAddress
+	envBase := cfg.BaseURL
+
+	// Переменные из флагов
+	addrFlag := flag.String("a", cfg.ServerAddress, "адрес запуска HTTP-сервера")
+	baseFlag := flag.String("b", cfg.BaseURL, "базовый адрес для сокращённых URL")
 
 	flag.Parse()
 
-	return &Config{
-		ServerAddress: *addr,
-		BaseURL:       *base,
+	// Если значение из env пустое — берём флаг
+	if envAddr == "" && addrFlag != nil {
+		cfg.ServerAddress = *addrFlag
 	}
+	if envBase == "" && baseFlag != nil {
+		cfg.BaseURL = *baseFlag
+	}
+
+	return cfg
 }
