4698202637f422746afe3979728ea8fb7e1b3eb8
Denis <cool-di@mail.ru>
2025-05-16 19:06:48 +0300

    iter6 Добавление middleware с логированием

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -3,21 +3,42 @@ package main
 import (
 	"github.com/divanov-web/shorturl/internal/config"
 	"github.com/divanov-web/shorturl/internal/handlers"
-	"log"
+	"github.com/divanov-web/shorturl/internal/middleware"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"go.uber.org/zap"
 )
 
 func main() {
 	cfg := config.NewConfig()
 
+	// создаём предустановленный регистратор zap
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		panic(err)
+	}
+	defer logger.Sync()
+	// делаем регистратор SugaredLogger
+	sugar := logger.Sugar()
+	middleware.SetLogger(sugar) // передаём логгер в middleware
+
 	h := handlers.NewHandler(cfg.BaseURL)
 
 	r := chi.NewRouter()
 
+	r.Use(middleware.WithLogging)
+
 	r.Post("/", h.MainPage)
 	r.Get("/{id}", h.GetRealURL)
 
-	log.Fatal(http.ListenAndServe(cfg.ServerAddress, r))
+	sugar.Infow(
+		"Starting server",
+		"addr", cfg.ServerAddress,
+	)
+
+	if err := http.ListenAndServe(cfg.ServerAddress, r); err != nil {
+		sugar.Fatalw("Server failed", "error", err)
+	}
+
 }
