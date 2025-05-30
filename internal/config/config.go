4c40444a5603bda0dbdc4d62bc758aa93bf8e8ca
Denis <cool-di@mail.ru>
2025-05-30 16:00:20 +0300

    iter10 пинг к БД

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"flag"
 	"github.com/caarlos0/env/v6"
+	"github.com/joho/godotenv"
 	"log"
 )
 
@@ -10,36 +11,36 @@ type Config struct {
 	ServerAddress   string `env:"SERVER_ADDRESS"`
 	BaseURL         string `env:"BASE_URL"`
 	FileStoragePath string `env:"FILE_STORAGE_PATH"`
+	DatabaseDSN     string `env:"DATABASE_DSN"`
 }
 
 func NewConfig() *Config {
+	// Загрузим .env только если переменные ещё не заданы в окружении
+	_ = godotenv.Load()
+
 	// флаги без дефолтов
 	addrFlag := flag.String("a", "", "адрес запуска HTTP-сервера")
 	baseFlag := flag.String("b", "", "базовый адрес для сокращённых URL")
 	filePathFlag := flag.String("f", "", "путь к файлу хранения данных")
+	dbDSNFlag := flag.String("d", "", "строка подключения к БД")
 
 	flag.Parse()
 
-	// переменные окружения
 	envCfg := Config{}
 	if err := env.Parse(&envCfg); err != nil {
 		log.Fatal(err)
 	}
 
-	// Формируем итоговую конфигурацию с приоритетами:
-	// 1. Переменная окружения
-	// 2. Флаг командной строки
-	// 3. Значение по умолчанию
 	cfg := &Config{
 		ServerAddress:   chooseValue(envCfg.ServerAddress, *addrFlag, "localhost:8080"),
 		BaseURL:         chooseValue(envCfg.BaseURL, *baseFlag, "http://localhost:8080"),
 		FileStoragePath: chooseValue(envCfg.FileStoragePath, *filePathFlag, "shortener_data.json"),
+		DatabaseDSN:     chooseValue(envCfg.DatabaseDSN, *dbDSNFlag, ""),
 	}
 
 	return cfg
 }
 
-// Вспомогательная функция для выбора значения
 func chooseValue(envVal, flagVal, defaultVal string) string {
 	if envVal != "" {
 		return envVal
